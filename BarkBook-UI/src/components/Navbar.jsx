79d1a8488bca2d360fd2fafcd44726a132ae6ff1
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-07-16 16:54:54 -0500

    implement Navbar.jsx
    Very simple, placeholder navbar. Iterates over objects in `destinations` variable. Paths may be added to variable as needed during development. Paths may be hidden from navbar by setting `valid` to `false`. End goal is to pull the paths needed directly from the DOM, but this will do fine for development convenience.

---
+++ BarkBook-UI/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+// edit and add destinations as needed
+// TODO: find paths dynamically with DOM
+const destinations = [
+    {name: 'home', href: '/', valid: true},
+    {name: 'login', href: '/login', valid: true},
+    {name: 'about', href: '/about', valid: true},
+    {name: 'noRender', href: '/notReady', valid: false},
+    {name: 'new link', href: '/newpage', valid: false},
+]
+
+// TODO: make navbar sticky
+const Navbar = () => {
+    return (
+        <div>
+            <div className={"flex flex-col items-center bg-gray-400"}>
+                <div className="space-x-4">
+                    {/*iterate over array of destination objects*/}
+                    {destinations.map((item) => (
+                        // if item.valid is true, render the button
+                        item.valid ? (
+                            <a
+                                key={item.name}
+                                href={item.href}
+                                className={'text-black bg-gray-200 hover:text-white hover:bg-black'}
+                            >
+                                | {item.name} |
+                            </a>
+                        ) : null
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
