cfc688c3dc04511b547c44a5d3b1c789ab5c24ba
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-07-16 14:43:20 -0500

    Remove default styling
    most of tailwindCSS is handled in `className` in any given .jsx element tree. I Removed vite default CSS to avoid confusion. Also removed vite junk from App.jsx, and left a small example in there of how to use tailwind. See their docs for classNames and more information https://tailwindcss.com/docs/

--- BarkBook-UI/src/App.jsx
+++ BarkBook-UI/src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className={"grid place-content-center h-80"}>
+            <p className={"line-through"}>Spend hours on CSS</p> <p className={"underline"}>Use Tailwind</p>
+                <p className="text-sky-400/100">The quick brown fox...</p>
+                <p className="text-sky-400/75">The quick brown fox...</p>
+                <p className="text-sky-400/50">The quick brown fox...</p>
+                <p className="text-sky-400/25">The quick brown fox...</p>
+                <p className="text-sky-400/0">The quick brown fox...</p></div>
     </>
   )
 }
