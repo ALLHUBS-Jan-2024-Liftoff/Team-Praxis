274726c28ff5a301bd88471c85766d4ab40383ce
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-07-17 20:12:35 -0500

    Commit before fetch

--- BarkBook-UI/src/App.jsx
+++ BarkBook-UI/src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from "./components/Navbar.jsx";
+import BarkBookLogin from "./components/BarkBookLogin.jsx"
+// import BarkBookRegistration from "./components/BarkBookRegistration.jsx";
 
-import TailwindExample from "./components/TailwindExample.jsx";
+// import TailwindExample from "./components/TailwindExample.jsx";
 
 const App = () => {
   return (
     <>
         <Navbar />
         {/*<TailwindExample />*/}
+        <BarkBookLogin />
+        {/* <BarkBookRegistration /> */}
 
     </>
   )
