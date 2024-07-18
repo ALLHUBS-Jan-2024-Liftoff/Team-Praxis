9db8c24916ed2b2f8abd9f3877ab38dfe0bcd3c7
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-07-17 19:31:02 -0500

    setup routing
    new routes can be set in the App.jsx variable `router`, as demonstrated in the code comments. Navbar is also adjusted to use the routing format `<Link to="path">` which is preferred to `<a>` and `href` with react-router-dom.

--- BarkBook-UI/src/components/Navbar.jsx
+++ BarkBook-UI/src/components/Navbar.jsx
@@ -1,33 +1,31 @@
-// edit and add destinations as needed
-// TODO: find paths dynamically with DOM
+import {Link} from "react-router-dom";
+
+// edit and add destinations as needed, for now
+// setting `valid` to false means it will not render in the navbar
 const destinations = [
-    {name: 'home', href: '/', valid: true},
-    {name: 'login', href: '/login', valid: true},
-    {name: 'about', href: '/about', valid: true},
-    {name: 'noRender', href: '/notReady', valid: false},
-    {name: 'new link', href: '/newpage', valid: false},
+    {name: 'home', path: '/', valid: true},
+    {name: 'test', path: '/test', valid: true},
+    {name: 'about', path: '/about', valid: true},
 ]
 
 // TODO: make navbar sticky
 const Navbar = () => {
     return (
-        <div>
-            <div className={"flex flex-col items-center bg-gray-400"}>
-                <div className="space-x-4">
-                    {/*iterate over array of destination objects*/}
-                    {destinations.map((item) => (
-                        // if item.valid is true, render the button
-                        item.valid ? (
-                            <a
-                                key={item.name}
-                                href={item.href}
-                                className={'text-black bg-gray-200 hover:text-white hover:bg-black'}
-                            >
-                                | {item.name} |
-                            </a>
-                        ) : null
-                    ))}
-                </div>
+        <div className={"flex flex-col items-center bg-gray-400"}>
+            <div className="space-x-4">
+                {/*iterate over array of destination objects*/}
+                {destinations.map((item) => (
+                    // if item.valid is true, render the button
+                    item.valid ? (
+                        <Link
+                            key={item.name}
+                            to={item.path}
+                            className={'text-black bg-gray-200 hover:text-white hover:bg-black'}
+                        >
+                            | {item.name} |
+                        </Link>
+                    ) : null
+                ))}
             </div>
         </div>
     )
---
+++ BarkBook-UI/src/pages/AboutPage.jsx
@@ -0,0 +1,5 @@
+export const AboutPage = () => {
+    return (
+        <>About me? A really neat app :)</>
+    )
+}
