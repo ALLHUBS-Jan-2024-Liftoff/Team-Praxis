37e285a5a75d633a52489e07ad4fb5eaae017de7
unknown <rahulpksu@gmail.com>
2024-07-18 21:03:18 -0500

    created controller and models page with details

---
+++ BarkBook-API/src/main/java/org/teampraxis/BarkBook_API/controllers/DogController.java
@@ -0,0 +1,9 @@
+package org.teampraxis.BarkBook_API.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class DogController {
+}
