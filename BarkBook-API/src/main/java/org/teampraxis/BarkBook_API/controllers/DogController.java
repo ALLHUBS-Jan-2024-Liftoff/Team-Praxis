9539918f9ccd62a0432c878fb417df39dc737d1c
unknown <rahulpksu@gmail.com>
2024-07-28 13:49:59 -0500

    edit and delete mapping completed

--- BarkBook-API/src/main/java/org/teampraxis/BarkBook_API/controllers/DogController.java
+++ BarkBook-API/src/main/java/org/teampraxis/BarkBook_API/controllers/DogController.java
@@ -9,17 +9,20 @@ import org.teampraxis.BarkBook_API.repositories.DogRepository;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:5173")
-@RestController // combines ResponseBody and Controller annotation. Used in RESTful web services. Automatically  converts return value of the methods to JSON
+@RestController // combines ResponseBody and Controller annotation. Used in restful web services. Automatically  converts return value of the methods to JSON
 public class DogController {
 
     @Autowired
     private DogRepository dogRepository;
 
+
+    // Used for adding a dog to database
     @PostMapping("/add-dog")
     public Dog addDog(@RequestBody Dog reqDog) {
         return dogRepository.save(reqDog);
     }
 
+    // both methods below used for displaying dog data
     @GetMapping("/users")
     List<Dog> getAllDogs() {
         return dogRepository.findAll();
@@ -29,5 +32,28 @@ public class DogController {
     public Dog getUserById(@PathVariable Integer id) {
         return dogRepository.findById(id)
                 .orElseThrow(() -> new DogNotFoundException(id));
+    }
+
+    // used for editing specific data dog by id
+    @PutMapping("/users/dog/{id}")
+    public Dog updateDogById(@RequestBody Dog reqDog, @PathVariable Integer id) {
+        return dogRepository.findById(id)
+                .map(dog -> {
+                    dog.setDogName(reqDog.getDogName());
+                    dog.setDogAge(reqDog.getDogAge());
+                    dog.setBreed(reqDog.getBreed());
+                    dog.setWeight(reqDog.getWeight());
+                    return dogRepository.save(dog);
+                }).orElseThrow(() -> new DogNotFoundException(id));
+    }
+
+    // used to delete dog by id
+    @DeleteMapping("/users/dog/{id}")
+    String deleteDogById(@PathVariable Integer id){
+        if(!dogRepository.existsById(id)){
+            throw new DogNotFoundException(id);
+        }
+        dogRepository.deleteById(id);
+        return  "Dog with id "+id+" has been deleted successfully.";
     }
 }
