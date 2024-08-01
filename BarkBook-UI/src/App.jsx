0be228871984f0a7c2b592831922b16157f44746
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-08-01 11:35:23 -0500

    implement auth front end

--- BarkBook-UI/src/App.jsx
+++ BarkBook-UI/src/App.jsx
@@ -7,17 +7,19 @@ import {
 import {MainLayout} from "./layouts/MainLayout.jsx";
 import {HomePage} from "./pages/HomePage.jsx";
 import {AboutPage} from "./pages/AboutPage.jsx";
-import UserLogin from "./pages/users-temp/UserLogin.jsx"
-import UserRegister from "./pages/users-temp/UserRegister.jsx"
+import UserLogin from "./pages/user/UserLogin.jsx"
+import UserRegister from "./pages/user/UserRegister.jsx"
 import CreateEventForm from "./pages/CreateEventForm.jsx";
 import AddDog from './pages/AddDog.jsx';
-import ViewAllUsers from "./pages/users-temp/tests/ViewAllUsers.jsx";
-import EditUser from "./pages/users-temp/tests/EditUser.jsx";
-import ViewUserById from "./pages/users-temp/tests/ViewUserById.jsx";
+import ViewAllUsers from "./pages/user/temp/ViewAllUsers.jsx";
+import EditUser from "./pages/user/EditUser.jsx";
+import ViewUserById from "./pages/user/temp/ViewUserById.jsx";
 import {AccountPage} from "./pages/AccountPage.jsx";
 import {MapContainer} from "./api/google/MapContainer.jsx";
 import ViewDog from './pages/ViewDog.jsx';
 import EditDog from './pages/EditDog.jsx';
+import {AuthProvider} from "./AuthContext.jsx";
+import {UserStuff} from "./pages/user/temp/UserStuff.jsx";
 
 
 
@@ -26,17 +28,18 @@ const router = createBrowserRouter(
         <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
             <Route path={"about"} element={<AboutPage />} />
+            <Route path={"userstuff"} element={<UserStuff />} />
             <Route path={"login"} element={<UserLogin />} />
             <Route path={"register"} element={<UserRegister />} />
+            <Route path={"allusers"} element={<ViewAllUsers />} />
+            <Route path={"viewuser/:id"} element={<ViewUserById />} />
+            <Route path={"user/:id"} element={<AccountPage />} />
+            <Route path={"user/:id/edit"} element={<EditUser />} />
             <Route path={"create-event"} element={<CreateEventForm />} />
             <Route path={"add-dog"} element={<AddDog />} />
-            <Route path={"allusers"} element={<ViewAllUsers />} />
-            <Route path={"edituser/:id"} element={<EditUser />} />
-            <Route path={"viewuser/:id"} element={<ViewUserById />} />
-            <Route path={"user"} element={<AccountPage />} />
-            <Route path={"map"} element={<MapContainer />} />
             <Route path={"/user/dog/:id"} element={<ViewDog />} />
             <Route path={"/user/edit-dog/:id"} element={<EditDog />} />
+            <Route path={"map"} element={<MapContainer />} />
             {/*To add a path, edit and uncomment...*/}
             {/*<Route path={"myPath"} element={<myPage />} />*/}
             {/*And add it to the Navbar array*/}
@@ -45,7 +48,11 @@ const router = createBrowserRouter(
 );
 
 const App = () => {
-    return <RouterProvider router={router} />;
+    return (
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    );
 }
 
 export default App
